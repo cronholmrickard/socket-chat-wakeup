3e5679b82bf6df09c140797ea7d6148f8de2b092
Rickard Cronholm <rickard.cronholm@northlink.io>
2024-08-16 16:23:09 +0200

    Added reconnection

--- server.js
+++ server.js
@@ -24,35 +24,43 @@ io.on('connection', (socket) => {
   logMessage('INFO', `Socket connected with id: ${socket.id}`);
 
   socket.on('join', (username) => {
-    // Check if user is already in the list
     let user = connectedUsers.find((u) => u.name === username);
+
     if (user) {
-      user.status = 'online'; // Update status to online
-      user.socketId = socket.id; // Update socket ID
+      // If user already exists, just update their socket ID and status to online
+      const time = moment().format('YYYY-MM-DD HH:mm:ss');
+      user.status = 'online';
+      user.socketId = socket.id;
+
+      // Notify others that the user rejoined
+      io.emit('userRejoined', { username, time });
+      logMessage('INFO', `${username} rejoined the chat`);
     } else {
+      // If user does not exist, add them to the list
+      const time = moment().format('YYYY-MM-DD HH:mm:ss');
       user = { name: username, status: 'online', socketId: socket.id };
       connectedUsers.push(user);
-    }
-    socket.username = username;
 
-    // Log when a user joins
-    logMessage('INFO', `${username} joined the chat`);
+      // Notify others that a new user joined
+      io.emit('userJoined', { username, time });
+      logMessage('INFO', `${username} joined the chat`);
+    }
+
+    socket.username = username;
 
     // Send the updated user list to the connected user
     socket.emit('userList', connectedUsers);
-
-    // Notify all users except the newly connected user about the new connection
-    socket.broadcast.emit('userJoined', username);
   });
 
   socket.on('disconnect', () => {
     const user = connectedUsers.find((u) => u.name === socket.username);
     if (user) {
+      const time = moment().format('YYYY-MM-DD HH:mm:ss');
       user.status = 'offline'; // Update status to offline
       logMessage('INFO', `${socket.username} disconnected`);
 
       // Notify all clients that a user has disconnected
-      io.emit('userDisconnected', socket.username);
+      io.emit('userDisconnected', { username: socket.username, time });
     }
   });
 
